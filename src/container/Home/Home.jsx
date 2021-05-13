5f71a8eef13a59be02fc6293f7dfae828696678d
laughface809 <akhmarullah.techno@gmail.com>
2021-05-14 00:57:38 +0800

    parent child component

---
+++ src/container/Home/Home.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import HeaderComponent from "../../component/HeaderComponent/HeaderComponent";
+import Product from "../Product/Product";
+
+class Home extends Component{
+    render() {
+        return(
+            <>
+                {/*<p>Header Component</p>
+                <hr />
+                <HeaderComponent
+                    time={"7.13"}
+                    title={"Feedback 1"}
+                    desc={"This is a great place"}
+                />
+                <HeaderComponent
+                    time={"5.12"}
+                    title={"Feedback 2"}
+                    desc={"This is a great time"}
+                />
+                <HeaderComponent
+                    time={"6.12"}
+                    title={"Feedback 3"}
+                    desc={"This is a great fun"}
+                />
+                <HeaderComponent/>*/}
+                <p>Subscribe</p>
+                <hr />
+                <Product />
+            </>
+        )
+    }
+}
+
+export default Home;
