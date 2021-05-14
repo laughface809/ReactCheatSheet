d3ccee7b1208fef400ef7a5df639b70f26b9a391
laughface809 <akhmarullah.techno@gmail.com>
2021-05-14 23:56:10 +0800

    put

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -11,7 +11,8 @@ class BlogPost extends Component{
             title: '',
             body: '',
             userId: 1,
-        }
+        },
+        isUpdate: false
     }
 
     getPostAPI = () => {
@@ -28,14 +29,50 @@ class BlogPost extends Component{
             console.log(result);
             alert('success add');
             this.getPostAPI();
+            this.setState({
+                isUpdate: false,
+                formBlogPost:{
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1,
+                },
+            })
         }, (err) => {
             console.log('error', err);
         })
     }
 
+    putDataToApi = () => {
+        axios.put(`http://localhost:8080/posts/${this.state.formBlogPost.id}`, this.state.formBlogPost).then((result)=>{
+            console.log(result)
+            alert('success update');
+            this.getPostAPI();
+            this.setState({
+                isUpdate: false,
+                formBlogPost:{
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1,
+                },
+            })
+        }, (err) => {
+            console.log('error', err);
+        })
+    }
+
+    handleUpdate = (data) => {
+        console.log(data);
+        this.setState({
+            formBlogPost: data,
+            isUpdate: true
+        })
+    }
+
     handleRemove = (data) => {
         console.log(data);
-        axios.delete(`http://localhost:8080/posts/${data}`).then((result)=>{
+        axios.delete(`http://localhost:8080/posts/${data}`, this.state.formBlogPost).then((result)=>{
             alert('success delete');
             this.getPostAPI();
         })
@@ -44,7 +81,9 @@ class BlogPost extends Component{
     handleFormChange = (event) => {
         let formBlogPostNew = {...this.state.formBlogPost};
         let timestamp = new Date().getTime();
-        formBlogPostNew['id'] = timestamp;
+        if(!this.state.isUpdate){
+            formBlogPostNew['id'] = timestamp;
+        }
         formBlogPostNew[event.target.name] = event.target.value;
         this.setState({
             formBlogPost: formBlogPostNew
@@ -52,7 +91,11 @@ class BlogPost extends Component{
     }
 
     handleSubmit = () => {
-        this.postDataToApi();
+        if(this.state.isUpdate){
+            this.putDataToApi();
+        }else {
+            this.postDataToApi();
+        }
     }
 
     componentDidMount() {
@@ -72,14 +115,14 @@ class BlogPost extends Component{
             <p className={"section-title"}>Blog Post</p>
                 <div className={"form-add-post"}>
                     <label htmlFor={"title"}>Title</label>
-                    <input type={"text"} name={"title"} placeholder={"add title"} onChange={this.handleFormChange} />
+                    <input type={"text"} name={"title"} placeholder={"add title"} onChange={this.handleFormChange} value={this.state.formBlogPost.title} />
                     <label htmlFor={"body"}>Blog Content</label>
-                    <textarea name={"body"} id={"body"} cols={"30"} rows={"10"} placeholder={"add content"} onChange={this.handleFormChange}/>
+                    <textarea name={"body"} id={"body"} cols={"30"} rows={"10"} placeholder={"add content"} onChange={this.handleFormChange} value={this.state.formBlogPost.body}/>
                     <button className={"btn-submit"} onClick={this.handleSubmit}>Simpan</button>
                 </div>
                 {
                     this.state.post.map(post=>{
-                        return <Post key={post.id} data={post} title={post.title} desc={post.body} remove={this.handleRemove}/>
+                        return <Post key={post.id} data={post} title={post.title} desc={post.body} remove={this.handleRemove} update={this.handleUpdate}/>
                     })
                 }
             </Fragment>
