bfa38df2c0c5617fcfc8a692423fd9ca977e0c60
laughface809 <akhmarullah.techno@gmail.com>
2021-05-14 14:26:22 +0800

    axios

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from "react";
 import './BlogPost.css';
 import Post from "../../component/Post/Post";
+import axios from "axios";
 
 class BlogPost extends Component{
     state = {
@@ -8,11 +9,17 @@ class BlogPost extends Component{
     }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        /*fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             .then(json => {
                 this.setState({
                     post: json
+                })
+            })*/
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((result) => {
+                this.setState({
+                    post: result.data
                 })
             })
     }
